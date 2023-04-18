23ff0bbf9ce3378ac1c4521c4fc6fd4e8719a441
Sergei <yagelavichus@gmail.com>
2023-04-18 19:35:47 +0200

    task 001 sum N

---
+++ Homeworks/Home_1/Task001.java
@@ -0,0 +1,15 @@
+// Вычислить n-ое треугольного число (сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
+
+ package Java_projects.Homeworks.Home_1;
+
+import java.util.Scanner;
+
+public class Task001 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число N: ");
+        double n = scanner.nextInt();
+        double res = (n / 2)*(n+1);
+        System.out.println(res);
+    }
+} 
