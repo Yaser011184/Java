25da75225927df73d32df3e77df501c7e9c9cbee
Sergei <yagelavichus@gmail.com>
2023-04-18 21:02:08 +0200

    task 001 add N! - Factorial

--- Homeworks/Home_1/Task001.java
+++ Homeworks/Home_1/Task001.java
@@ -1,15 +1,23 @@
 // Вычислить n-ое треугольного число (сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
 
- package Java_projects.Homeworks.Home_1;
+package Java_projects.Homeworks.Home_1;
 
 import java.util.Scanner;
 
 public class Task001 {
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите число N: ");
         double n = scanner.nextInt();
-        double res = (n / 2)*(n+1);
+        double res = (n / 2) * (n + 1);
         System.out.println(res);
+        int fact = 1;
+        for (int i = 2; i <= n; i++) {
+            fact = fact * i;
+        }
+
+        System.out.println(fact);
+
     }
-} 
+}
